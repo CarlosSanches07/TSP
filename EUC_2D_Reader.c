00d63d339f6bcc14d442bde5b195e9a4552ee183
Carlos Sanches <carlos-caps@hotmail.com>
2017-06-17 15:32:43 -0300

    Add files via upload

--- EUC_2D_Reader.c
+++ EUC_2D_Reader.c
@@ -1,4 +1,6 @@
 #include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 typedef struct euc_2d
 {
@@ -15,24 +17,29 @@ typedef struct queue
 }Queue;
 
 Queue* create(){
-	return (Queue*)malloc(sizeof(Queue));
+	Queue* Q = (Queue*)malloc(sizeof(Queue));
+	Q->start = NULL;
+	Q->end = NULL;
+	Q->size = 0;
+	return Q;
 }
 
 void insertQueue(Queue* Q,int city, float x, float y){
 	EUC_2D* euc;
-	euc  =(EUC_2D*)malloc(sizeof(EUC_2D));
+	euc =(EUC_2D*)malloc(sizeof(EUC_2D));
 	euc->city = city;
 	euc->x = x;
 	euc->y = y;
-
-	if(Q->start!=NULL){
-		Q->end->next = euc;
-		Q->end = euc;
-		Q->size++;
-	}else{
+	if(Q->start == NULL){
 		Q->start = euc;
 		Q->end = euc;
+		euc->next = NULL;
 		Q->size = 0;
+	}else{
+		Q->end->next = euc;
+		euc->next = NULL;
+		Q->end = euc;
+		Q->size ++;
 	}
 }
 
@@ -62,13 +69,15 @@ Queue* alloc2D(FILE* tsp){
 		Inserts the coordinates of each city in the queue
 	*/
 
-	while(fscanf(tsp, "%s %s %s", city, x, y)!= EOF)
+	while(fscanf(tsp, "%s %s %s", city, x, y)!= EOF){
+		printf("go");
 		insertQueue(Q, atoi(city), atof(x), atof(y));
+	}
 
 	return Q;
 }
 
-EUC_2D* search_city(Queue Q, int city){//Search city by index
+EUC_2D* search_city(Queue* Q, int city){//Search city by index
 	EUC_2D* euc = Q->start;
 
 	while(euc != NULL && euc->city != city)
@@ -87,21 +96,22 @@ float calc_distance_EUC_2D(Queue* Q, int c1, int c2){//Calculate the distance be
 
 void show_cost_matrix_2D(float** matrix, int size){
 	int i, j;
+	printf("\n");
 	for(i = 0; i < size; i++){
 		for(j = 0; j < size; j++)
-			printf("%.3f \t",matrix[i][j]);
+			printf("%4.0f ",matrix[i][j]);
 		printf("\n");
 	}
 }
 
-void cost_matrix_2D(Queue Q){
+void cost_matrix_2D(Queue* Q){
 	int i,j;
-
+	printf("%d",Q->size);
 	float** matrix =(float**)malloc(Q->size * sizeof(float*));
-
+	printf("%d",Q->size);
 	for(i = 0; i < Q->size; i++)
-		matrix[i] =(float*)malloc(data->size * sizeof(float));
-
+		matrix[i] =(float*)malloc(Q->size * sizeof(float));
+		
 	for(i = 0; i < Q->size; i++){
 		for(j = 0; j < Q->size; j++){
 			if(i == j){
--- main.c
+++ main.c
@@ -1,18 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <conio.h>
 
 #include "EUC_2D_Reader.c"
 
 main(){
 	Queue* Q = NULL;// A queue to alloc the data
 
-	char path[50] = "\\libs\\";//Standard path of the libs
+	char path[50] = "libs\\";//"libs\\";//Standard path of the libs
 	char file_name[50];
 
-	printf("What tsp data file you want to read ?\n(type the name of file ex:\"kroA100.tsp\")");
+	printf("What tsp data file do you want to read ?\n(type the name of file ex:\"kroA100.tsp\")");
 	gets(file_name);//Get the  .tsp file name
-	path = strcat(path,file_name); //Concat the standard path to the file name
+	strcat(path,file_name); //Concat the standard path to the file name
 
 	FILE* file = fopen(path, "r");//Open the file to readd
 
@@ -31,7 +32,7 @@ main(){
 	if(Q != NULL){
 		printf("Queue allocated!! Starting solve... %d",Q->size);
 
-		const_matrix_2D(Q);//Build a matrix with the distances between cities
+		cost_matrix_2D(Q);//Build a matrix with the distances between cities
 	}else{
 		printf("failed to alloc");
 	}
